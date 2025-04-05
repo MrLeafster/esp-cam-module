50bcfeacf5cbed195c5f4cb6eb77742ea822d791
Luka Bradarić Lisić <starwarsjedobar@gmail.com>
2025-04-05 23:43:13 +0200

    Add initial sdcard driver
    - add initial version of the sdcard driver

---
+++ components/sdcard/sdcard.c
@@ -0,0 +1,112 @@
+/*******************************************************************************/
+/*                                  INCLUDES                                   */
+/*******************************************************************************/
+
+#include "sdcard.h"
+
+#include <stdio.h>
+
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+
+#include "esp_log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*******************************************************************************/
+/*                                   MACROS                                     */
+/*******************************************************************************/
+
+/*******************************************************************************/
+/*                                 DATA TYPES                                  */
+/*******************************************************************************/
+
+/*******************************************************************************/
+/*                         PRIVATE FUNCTION PROTOTYPES                         */
+/*******************************************************************************/
+
+/*******************************************************************************/
+/*                          STATIC DATA & CONSTANTS                            */
+/*******************************************************************************/
+
+/*******************************************************************************/
+/*                                 GLOBAL DATA                                 */
+/*******************************************************************************/
+
+/*******************************************************************************/
+/*                              PUBLIC FUNCTIONS                               */
+/*******************************************************************************/
+
+void sdcard_init()
+{
+    esp_err_t ret;
+
+    // Options for mounting the filesystem.
+    // If format_if_mount_failed is set to true, SD card will be partitioned and
+    // formatted in case when mounting fails.
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t *card;
+    const char mount_point[] = SDCARD_MOUNT_POINT;
+    ESP_LOGI(SDCARD_TAG, "Initializing SD card");
+
+    // Use settings defined above to initialize SD card and mount FAT filesystem.
+    // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
+    // Please check its source code and implement error recovery when developing
+    // production applications.
+    ESP_LOGI(SDCARD_TAG, "Using SPI peripheral");
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    spi_bus_config_t bus_cfg = {
+        .mosi_io_num = SDCARD_MOSI,
+        .miso_io_num = SDCARD_MISO,
+        .sclk_io_num = SDCARD_SCLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+    ret = spi_bus_initialize(host.slot, &bus_cfg, SDSPI_DEFAULT_DMA);
+    if (ret != ESP_OK) {
+        ESP_LOGE(SDCARD_TAG, "Failed to initialize bus.");
+        return;
+    }
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = SDCARD_CS;
+    slot_config.host_id = host.slot;
+
+    ESP_LOGI(SDCARD_TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(SDCARD_TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(SDCARD_TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(SDCARD_TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+}
+
+/*******************************************************************************/
+/*                             PRIVATE FUNCTIONS                               */
+/*******************************************************************************/
+
+/*******************************************************************************/
+/*                             INTERRUPT HANDLERS                              */
+/*******************************************************************************/
